71cfc46103ca05c593f9115bedae6a0c2e5b636b
Raza2468 <faizeraza2468@gamil.com>
2021-07-29 00:49:29 +0500

    ch

--- Server/dberor/models.js
+++ Server/dberor/models.js
@@ -5,7 +5,10 @@ const mongoose = require("mongoose");
 // let dbURI = "mongodb+srv://dbuser:dbpassword@cluster0.9qvbs.mongodb.net/abc-database";
 let dbURI = "mongodb+srv://faiz:2468@mundodb.lkd4g.mongodb.net/ttest?retryWrites=true&w=majority";
 // let dbURI = 'mongodb://localhost:27017/abc-database';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useFindAndModify',false);
+mongoose.Promise=global.Promise;
+
+mongoose.connect(dbURI ||'mongodb://localhost:27017/suripanta_rest', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 ////////////////mongodb connected disconnected events///////////////////////////////////////////////
