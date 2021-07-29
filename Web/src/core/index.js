81a1e3929c0b7acfefd34bac61875675a490abfd
Raza2468 <faizeraza2468@gamil.com>
2021-07-30 01:09:55 +0500

    herokuDeploy

---
+++ Web/src/core/index.js
@@ -0,0 +1,12 @@
+
+// var url = window.location.href.split(":");
+
+// if (url[0]==="https")
+// {
+//     url = "https://pro-mern-login.herokuapp.com/"
+// }
+// else{
+//     url = "http://localhost:3001"
+// }
+
+
