6b3d8046af0a347c368ceb16b7ab911c20be8792
Raza2468 <faizeraza2468@gamil.com>
2021-07-30 01:20:56 +0500

    pus

--- Web/src/core/index.js
+++ Web/src/core/index.js
@@ -8,5 +8,14 @@
 // else{
 //     url = "http://localhost:3001"
 // }
+// var url = window.location.href.split(":");
+
+// if (url[0]==="https")
+// {
+//     url = "https://pro-mern-login.herokuapp.com/"
+// }
+// else{
+//     url = "http://localhost:3001"
+// }
 
 
