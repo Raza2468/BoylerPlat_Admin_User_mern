7212f13f40044b2c7c556d5473fa8336907e9e1a
Raza2468 <faizeraza2468@gamil.com>
2021-08-02 21:58:51 +0500

    che

--- Web/src/Context/globaleContext.jsx
+++ Web/src/Context/globaleContext.jsx
@@ -1,12 +1,19 @@
 import React, { useContext, useState, useEffect } from "react";
 import axios from 'axios'
+
+// Allowing credentials true for axios 
+axios.defaults.withCredentials = true;
+
+// Calling global context from React
 // import url from "../core/index";
 const GlobalStateContext = React.createContext()
 const GlobalStateUpdateContext = React.createContext()
 
+// Creating global Context
 export const useGlobalState = () => useContext(GlobalStateContext)
 export const useGlobalStateUpdate = () => useContext(GlobalStateUpdateContext)
 
+// Making Global State component
 export function GlobalStateProvider({ children }) {
     const url = "http://localhost:3001";
     const [data, setData] = useState({
@@ -19,22 +26,22 @@ export function GlobalStateProvider({ children }) {
         axios({
             method: "get",
             url: url + `/getProfile`,
-            withCredentials: true
+            // withCredentials: true
         }).then((res) => {
             console.log("context response", res.data.profile, res.status);
             if (res.status === 200) {
 
                 setData((prev) => ({
                     ...prev,
-                    user: res.data.profile,
                     loginStatus: true,
+                    user: res.data.profile,
                     role: res.data.profile.role
                 }))
                 console.log('check', res.data.profile.role)
             }
         }).catch((err) => {
             // return setData((prev) => ({ ...prev, role: "loggedout" }));
-            setData((prev) => ({ ...prev, loginStatus: true, user: null, role: null, role: "loggedout" }))
+            setData((prev) => ({ ...prev, loginStatus: false, user: null, role: null, role: "loggedout" }))
             console.log(err,"loggedout err");
             // console.log(JSON.stringify("Error===========", err.response.status));
             // console.log(err);
