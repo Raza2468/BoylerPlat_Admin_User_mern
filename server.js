a6b404f18045b4b3834d762d8168828bd17116ed
Raza2468 <faizeraza2468@gamil.com>
2021-08-02 17:39:14 +0500

    cheakDeplo

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var io = socketIo(server, { cors: { origin: "*", methods: "*", } });
 appxml.use(bodyParser.json());
 appxml.use(cookieParser());
 appxml.use(cors({
-    origin: ["http://localhost:3000", 'https://pro-mern-login.herokuapp.com'],
+    origin: ["http://localhost:3000", 'https://sweet-website.herokuapp.com'],
     // origin: ["http://localhost:3000", 'https://databaselogin.herokuapp.com/'],
     // origin: '*',
     credentials: true
